01fe4d2cd763395e453215fda12097e4023395a5
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2018-10-18 04:03:38 -0300

    New protocol with checksum
    Verify if the received value is equal to it's checksum before accepting it.

--- app/src/main/java/whitesun/telemetry/Protocolo.java
+++ app/src/main/java/whitesun/telemetry/Protocolo.java
@@ -8,65 +8,75 @@ public class Protocolo {
     List<Dado> dados = new ArrayList<Dado>();
 
     public String processaInput(String input) {
+        String parteValor = input;
+        String parteChecksum = input;
         String processado = input;
+        String itemDetectado = input;
+
         if (input.equals("")) {
-            return "";
+            return input;
         }
 
         int posicaoInicio = input.indexOf("!");
 
         if (posicaoInicio == -1) {
-            // Não encontrou nenhuma marcação de inicio
             return input;
         } else  {
-            // Se encontrou o caractere de inicio, apaga tudo que tem atras
-            input = input.substring(posicaoInicio);
+            input = input.substring(posicaoInicio+1);
         }
+
         int posicaoFim = input.indexOf("@");
 
         if (posicaoFim == -1) {
-            // Não encontrou fim, retorna do jeito que ta
             return input;
+        } else {
+            itemDetectado = input.substring(0, posicaoFim);
+
+            processado = input.substring(posicaoFim+1);
         }
 
-        // Captura o item mandado
-        String itemDetectado = input.substring(1, posicaoFim);
-        // Resto do buffer não processado
-        processado = input.substring(posicaoFim+1);
+        int posicaoSeparador = itemDetectado.indexOf(';');
 
-        int posicaoIgual = itemDetectado.indexOf("=");
+        if (posicaoSeparador == -1) {
+            return input;
+        } else {
+            parteValor = itemDetectado.substring(0, posicaoSeparador);
+            parteChecksum = itemDetectado.substring(posicaoSeparador+1);
+        }
 
-        if (posicaoIgual != -1) {
-            // Se encontrou um "="
-            String apelido = itemDetectado.substring(0, posicaoIgual);
+        int posicaoIgualValor = parteValor.indexOf("=");
+        int posicaoIgualChecksum = parteChecksum.indexOf("=");
+
+        if (posicaoIgualValor != -1 && posicaoIgualChecksum != -1) {
+            String apelido = parteValor.substring(0, posicaoIgualValor);
             boolean apelidoOK = false;
             boolean valorOK = false;
-            // O apelido deve ter 3 letras
             if (apelido.length() == 3) {
-                // Só deve conter letras
                 apelidoOK = true;
             }
+            String valor = parteValor.substring(posicaoIgualValor+1);
+            String checksum = parteChecksum.substring(posicaoIgualChecksum+1);
 
-            // Pega valor do item
-            String valor = itemDetectado.substring(posicaoIgual+1);
-            float fvalor = 0;
+            float fValor = 0;
+            float fChecksum = 0;
             try {
-                // Verifica se ele é convertível em float
-                fvalor = Float.parseFloat(valor);
+                fValor = Float.parseFloat(valor);
+                fChecksum = Float.parseFloat(checksum);
                 valorOK = true;
             } catch(NumberFormatException e) {
                 valorOK = false;
             }
-            if (apelidoOK && valorOK) {
-                atualizaDados(apelido, fvalor);
+
+            if (apelidoOK && valorOK && fValor==fChecksum) {
+                atualizaDados(apelido, fValor);
             }
         }
 
-        // Se sobrou mais algum começo, chama a função de novo
         if (input.indexOf("!") != -1) {
             return processaInput(processado);
             // TODO: Verificar com o Mariga se isso aqui nao gera um problema de por exemplo nao retornar o primeiro dado processado
         }
+
         return processado;
         // TODO: Verificar com o Mariga se essa funçao nao ta retornando lixo, isto é, caso a condiçao de dado ideal seja
         // encontrada ("!apelido=valor@") nao resta nada na string "processado".
